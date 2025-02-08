3cc5ca1af3f3072c85b0470bde9af8681e602b43
Elöd Dobos <elod.dobos@gmail.com>
2025-02-08 17:22:45 +0100

    Initial Commit

---
+++ CounterSwiftUI/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  CounterSwiftUI
+//
+//  Created by eloddobos on 2025-02-08.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ CounterSwiftUI/CounterSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  CounterSwiftUIApp.swift
+//  CounterSwiftUI
+//
+//  Created by eloddobos on 2025-02-08.
+//
+
+import SwiftUI
+
+@main
+struct CounterSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
