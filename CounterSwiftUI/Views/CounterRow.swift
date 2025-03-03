53f39ea8d7322d623712b87e6f7d2009bfacf259
Elöd Dobos <elod.dobos@gmail.com>
2025-03-03 09:32:57 +0100

    Added new line

--- CounterSwiftUI/Views/CounterRow.swift
+++ CounterSwiftUI/Views/CounterRow.swift
@@ -11,7 +11,7 @@ struct CounterRow: View {
     @Bindable var counter: CounterModel
 
     var body: some View {
-        NavigationLink("Name:\(counter.name) Count:\(counter.countString()) Date:\(counter.date.formatted(date: .abbreviated, time: .shortened))", destination: CounterInnerView(viewModel: CounterViewModel(counter: self.counter)))
+        NavigationLink("Name:\(counter.name)\n Count:\(counter.countString())\n Date:\(counter.date.formatted(date: .abbreviated, time: .shortened))", destination: CounterInnerView(viewModel: CounterViewModel(counter: self.counter)))
     }
 }
 
